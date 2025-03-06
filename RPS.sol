0feb09cf61d0b56b50039e51a913275c87d042ab
besterOz <bestnoppanut@gmail.com>
2025-03-06 22:57:09 +0700

    RPS + CommitReveal + TimeUnit

--- RPS.sol
+++ RPS.sol
@@ -1,21 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.2 <0.9.0;
 
-contract RPSLS_CommitReveal {
+contract RPSLS {
     uint public numPlayer = 0;
     uint public reward = 0;
-    
-    struct Commit {
-        bytes32 commitHash;
-        uint choice;
-        bool hasCommitted;
-        bool hasRevealed;
-    }
-
-    mapping(address => Commit) public commits;
+    mapping(address => bytes32) public player_commit;
+    mapping(address => uint) public player_choice;
+    mapping(address => bool) public player_not_played;
     address[] public players;
-    uint public numCommit = 0;
-    uint public numReveal = 0;
+    uint public numInput = 0;
+    uint256 public startTime;
+    uint256 public constant TIMEOUT = 5 minutes;
 
     address[4] private allowedPlayers = [
         0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,
@@ -38,54 +33,48 @@ contract RPSLS_CommitReveal {
 
     function addPlayer() public payable onlyAllowedPlayers {
         require(numPlayer < 2, "Game is full");
-        require(!commits[msg.sender].hasCommitted, "Player already in game");
+        if (numPlayer > 0) {
+            require(msg.sender != players[0], "Player already in game");
+        }
         require(msg.value == 1 ether, "Must send 1 ether");
-
+        
         reward += msg.value;
+        player_not_played[msg.sender] = true;
         players.push(msg.sender);
         numPlayer++;
-    }
-
-    function commit(bytes32 dataHash) public onlyAllowedPlayers {
-        require(numPlayer == 2, "Not enough players");
-        require(!commits[msg.sender].hasCommitted, "Already committed");
-
-        commits[msg.sender].commitHash = dataHash;
-        commits[msg.sender].hasCommitted = true;
-        numCommit++;
-
-        if (numCommit == 2) {
-            emit AllPlayersCommitted();
+        
+        if (numPlayer == 2) {
+            startTime = block.timestamp;
         }
     }
 
-    event AllPlayersCommitted();
+    function commit(bytes32 commitHash) public onlyAllowedPlayers {
+        require(player_not_played[msg.sender], "Already committed");
+        player_commit[msg.sender] = commitHash;
+    }
 
     function reveal(uint choice, bytes32 secret) public onlyAllowedPlayers {
-        require(numCommit == 2, "Both players must commit first");
-        require(!commits[msg.sender].hasRevealed, "Already revealed");
+        require(player_commit[msg.sender] != 0, "No commit found");
+        require(keccak256(abi.encodePacked(choice, secret)) == player_commit[msg.sender], "Commit does not match");
         require(choice >= 0 && choice <= 4, "Invalid choice");
-
-        bytes32 calculatedHash = keccak256(abi.encodePacked(choice, secret));
-        require(commits[msg.sender].commitHash == calculatedHash, "Invalid reveal");
-
-        commits[msg.sender].choice = choice;
-        commits[msg.sender].hasRevealed = true;
-        numReveal++;
-
-        if (numReveal == 2) {
+        
+        player_choice[msg.sender] = choice;
+        player_not_played[msg.sender] = false;
+        numInput++;
+        
+        if (numInput == 2) {
             _checkWinnerAndPay();
         }
     }
 
     function _checkWinnerAndPay() private {
-        uint p0Choice = commits[players[0]].choice;
-        uint p1Choice = commits[players[1]].choice;
+        uint p0Choice = player_choice[players[0]];
+        uint p1Choice = player_choice[players[1]];
         address payable account0 = payable(players[0]);
         address payable account1 = payable(players[1]);
-
+    
         uint result = (p0Choice - p1Choice + 5) % 5;
-
+    
         if (result == 1 || result == 3) {
             account0.transfer(reward);
         } else if (result == 2 || result == 4) {
@@ -94,21 +83,34 @@ contract RPSLS_CommitReveal {
             account0.transfer(reward / 2);
             account1.transfer(reward / 2);
         }
+    
+        _resetGame();
+    }
 
+    function forceWithdraw() public onlyAllowedPlayers {
+        require(numPlayer == 2, "Game not started");
+        require(block.timestamp > startTime + TIMEOUT, "Cannot withdraw yet");
+
+        if (player_not_played[players[0]]) {
+            payable(players[1]).transfer(reward);
+        } else if (player_not_played[players[1]]) {
+            payable(players[0]).transfer(reward);
+        }
+        
         _resetGame();
     }
 
     function _resetGame() private {
-        delete commits[players[0]];
-        delete commits[players[1]];
+        delete player_commit[players[0]];
+        delete player_commit[players[1]];
+        delete player_choice[players[0]];
+        delete player_choice[players[1]];
+        delete player_not_played[players[0]];
+        delete player_not_played[players[1]];
         delete players;
         numPlayer = 0;
-        numCommit = 0;
-        numReveal = 0;
+        numInput = 0;
         reward = 0;
-    }
-
-    function getHash(uint choice, bytes32 secret) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(choice, secret));
+        startTime = 0;
     }
 }
